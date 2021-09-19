c56afef3da36f0890f96baad1d9690ea1d7c73b8
rainaweinberg <raina.weinberg@ssfs.org>
2021-09-19 15:21:18 -0400

    Having trouble running, but finished painting code

--- src/PaintingARoom.java
+++ src/PaintingARoom.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
+
 public class PaintingARoom {
 
     public static void main(String[] args) {
-
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter room length");
+        int length = scan.nextInt();
+        System.out.println("Enter room width");
+        int width = scan.nextInt();
+        System.out.println("Enter room height");
+        int height = scan.nextInt();
+        System.out.println("How many doors are in the room?");
+        int doors = (15 * (scan.nextInt()));
+        System.out.println("How many windows?");
+        int windows = (10 * (scan.nextInt()));
+        System.out.println("How many coats of paint?");
+        int coats = scan.nextInt();
+        int surfaceArea = (2 * ((length * width) + (width * height) + (length * height)));
+        int totalFt = (surfaceArea) - (doors + windows);
+        double gallons = (coats * (totalFt/350));
+        int b = 10;
+        int a = (int) (gallons * 10);
+        int roundedGallons = ((a + b - 1) / b);
+        System.out.print("You need" + roundedGallons + "gallons of paint");
     }
-
 }
