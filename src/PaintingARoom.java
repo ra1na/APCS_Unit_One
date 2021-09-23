8d78260d69e63b3484c179faf96c7a3a9cc0bd4d
rainaweinberg <raina.weinberg@ssfs.org>
2021-09-23 18:38:07 -0400

    re-submitting w/ errors fixed (i think)

--- src/PaintingARoom.java
+++ src/PaintingARoom.java
@@ -5,19 +5,22 @@ public class PaintingARoom {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter room length");
-        int length = scan.nextInt();
+        double length = scan.nextDouble();
         System.out.println("Enter room width");
-        int width = scan.nextInt();
+        double width = scan.nextDouble();
         System.out.println("Enter room height");
-        int height = scan.nextInt();
+        double height = scan.nextDouble();
         System.out.println("How many doors are in the room?");
         int doors = (15 * (scan.nextInt()));
         System.out.println("How many windows?");
         int windows = (10 * (scan.nextInt()));
         System.out.println("How many coats of paint?");
-        int coats = scan.nextInt();
-        int surfaceArea = (2 * ((length * width) + (width * height) + (length * height)));
-        int totalFt = (surfaceArea) - (doors + windows);
+        final int coats = scan.nextInt();
+        double wall1 = (length * height);
+        double wall2 = (width * height);
+        double ceiling = (length * width);
+        double surfaceArea = (2 * wall1) + (2 * wall2) + ceiling;
+        double totalFt = (surfaceArea) - (doors + windows);
         double gallons = (coats * (totalFt/350));
         int b = 10;
         int a = (int) (gallons * 10);
